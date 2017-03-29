24d10513a40ab71b23b8cdfa1312d113112dd408
John Jairo Gomez <John Jairo Gomez>
2017-03-29 13:53:55 -0500

    Initial commit

---
+++ Assets/Scripts/AplicadorFuerzaArriba.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AplicadorFuerzaArriba : MonoBehaviour {
+
+	private float fuerzaaaa = 20f;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			other.gameObject.GetComponent<Rigidbody2D> ().AddForce (Vector3.up * fuerzaaaa, ForceMode2D.Impulse);
+		}
+	}
+
+}
---
+++ Assets/Scripts/GeneradorEnemigos.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneradorEnemigos : MonoBehaviour {
+
+	public GameObject enemigoPrefab;
+
+	private GameObject[] misEnemigos = new GameObject[5];
+
+	public Transform[] spawnPoints;
+
+	public Color[] misColores;
+ 
+	// Use this for initialization
+	void Start () {
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+		
+			CrearEnemigos ();
+
+		}
+
+	}
+
+	void CrearEnemigos ()
+	{
+		for (int i = 0; i < misEnemigos.Length; i++) {
+			int numeroAleatorio = Random.Range (0, spawnPoints.Length); 
+			misEnemigos[i] = Instantiate (enemigoPrefab, spawnPoints[numeroAleatorio].position, spawnPoints[i].rotation);
+			SpriteRenderer spriteTemporal = misEnemigos [i].GetComponent<SpriteRenderer> ();
+			int numeroAleatorio2 = Random.Range (0, misColores.Length);
+			spriteTemporal.color = misColores[numeroAleatorio2];
+		}
+	}
+}
